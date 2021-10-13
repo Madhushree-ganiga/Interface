b5874b081f83bbb3d1dd31fd0d1d64d6f1471278
Madhushree Ganiga/Javacode-Wolken <92204525+Madhushree-ganiga@users.noreply.github.com>
2021-10-13 12:26:05 +0530

    Add files via upload

---
+++ Tester.java
@@ -0,0 +1,12 @@
+class Tester{
+public static void main(String[] madhu)
+{
+Vehicle vehicle=new Benz();
+vehicle.travel();
+
+Gears gears=new Benz();
+gears.provideSafty();
+gears.getBrand();
+
+}
+}
